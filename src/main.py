cd89b07e3167c77bd9c804cbe24d76d3a4090456
sariya <dayzwaitik123@yandex.ru>
2024-03-03 22:54:09 +0300

    Change path to static

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from home.router import router as router_info
 from weather.router import router as router_weather
 
 app = FastAPI(title="Weather App")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/src/static", StaticFiles(directory="static"), name="static")
 
 
 app.include_router(router_info)
