ec9310396df111fd83a3b4f0a188b3093d612741
sariya <dayzwaitik123@yandex.ru>
2024-03-03 13:58:47 +0300

    Catch errors

--- src/weather/router.py
+++ src/weather/router.py
@@ -20,26 +20,47 @@ template = Jinja2Templates(directory="templates")
 
 @dataclass(frozen=True, slots=True)
 class Weather:
-    location: str
-    temperature: str
-    wind_speed: str
-    description: str
+    location: str = ''
+    temperature: str = ''
+    wind_speed: str = ''
+    description: str = ''
 
 
-def get_weather_by_city(city: str, lang: str = "ru", units: str = "metric") -> Weather:
+@dataclass(frozen=True, slots=True)
+class Response:
+    weather: Weather
+    status_code: int
+    error_message: str
+
+
+def get_weather_by_city(city: str, lang: str = "ru", units: str = "metric") -> Response:
     try:
         response = requests.get(f"{BASE_URL}q={city}&appid={API_KEY}&lang={lang}&units={units}")
         result_data = response.json()
-        logging.info(result_data)
         result = {
             "location": city,
             "temperature": result_data["main"]["temp"],
             "wind_speed": result_data["wind"]["speed"],
             "description": result_data["weather"][0]["description"],
         }
-        return Weather(**result)
-    except Exception as e:
-        raise e 
+        return Response(
+            weather=Weather(**result),
+            status_code=response.status_code,
+            error_message=response.reason
+        )
+    except requests.exceptions.RequestException:
+        return Response(
+            weather=Weather(),
+            status_code=400,
+            error_message="Rquest error"
+        )
+    except KeyError:
+        return Response(
+            weather=Weather(),
+            status_code=500,
+            error_message="KeyError"
+        )
+        
     
 
 
@@ -52,4 +73,6 @@ def weather_page(request: Request) -> HTMLResponse:
 def get_weather(request: Request, data: str = Form(...)) -> HTMLResponse:
     weather = get_weather_by_city(data)
     return template.TemplateResponse("weather.html", {"request": request, "page_name": "weather", "result": weather})
+
+
  
