cf9d79864a4f8e7df62acf9dda8edee603aa2f28
sariya <dayzwaitik123@yandex.ru>
2024-03-06 19:29:50 +0300

    Return separated date and time

--- src/weather/utils.py
+++ src/weather/utils.py
@@ -1,12 +1,13 @@
-from datetime import timedelta, datetime
+from datetime import timedelta, datetime, date, time
 from typing import TypeAlias
 
 SecondsShift: TypeAlias = int
 
 
-def get_datetime_by_utc_shift(date_time: datetime, shift: SecondsShift) -> datetime:
+def get_datetime_by_utc_shift(date_time: datetime, shift: SecondsShift) -> tuple[date, time]:
     """
     Добавляет к переданному времени сдвиг относительно UTC.
     """
     shift_in_hours = timedelta(hours=shift / 3600)
-    return (date_time + shift_in_hours)
+    now_datetime_utc = date_time + shift_in_hours
+    return now_datetime_utc.date(), now_datetime_utc.time()
