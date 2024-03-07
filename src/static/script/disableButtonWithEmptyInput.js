295b9709faeb10d4fb50c9401b9d766570b0d455
sariya <dayzwaitik123@yandex.ru>
2024-03-07 23:12:31 +0300

    No function

--- src/static/script/disableButtonWithEmptyInput.js
+++ src/static/script/disableButtonWithEmptyInput.js
@@ -1,12 +1,10 @@
-function disableButtonWithEmtyInput() {
-    const sendCity = document.getElementById("sendCity");
-    const cityInput = document.getElementById("floatingInput");
+const sendCity = document.getElementById("sendCity");
+const cityInput = document.getElementById("floatingInput");
 
-    cityInput.oninput = function () {
-        if (cityInput.value.length === 0) {
-            sendCity.disabled = true;
-        } else {
-            sendCity.disabled = false;
-        }
+cityInput.oninput = function () {
+    if (cityInput.value.length === 0) {
+        sendCity.disabled = true;
+    } else {
+        sendCity.disabled = false;
     }
 }
