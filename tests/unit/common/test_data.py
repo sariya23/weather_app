d32820c8902250759e079c9dacc1dc1178a85b28
sariya <dayzwaitik123@yandex.ru>
2024-03-08 16:43:12 +0300

    Add bound values

--- tests/unit/common/test_data.py
+++ tests/unit/common/test_data.py
@@ -55,5 +55,13 @@ WEATHER_DESCRIPTION = (
     "clear sky"
 )
 
-NIGHT_TIME = [time(hour=h) for h in (0, 1, 2, 3, 4, 5, 6, 7, 19, 20, 21, 22, 23)]
-DAY_TIME = [time(hour=h) for h in range(8, 19)]
+NIGHT_TIME_BOUNDARY = [
+    time(hour=7, minute=59),
+]
+
+DAY_TIME_BOUNDARU = [
+    time(hour=18, minute=59),
+]
+
+NIGHT_TIME = [time(hour=h) for h in (0, 1, 2, 3, 4, 5, 6, 7, 19, 20, 21, 22, 23)] + NIGHT_TIME_BOUNDARY
+DAY_TIME = [time(hour=h) for h in range(8, 19)] + DAY_TIME_BOUNDARU
