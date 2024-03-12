00fe65ff0f5d04bbc54a3fb255df38bf6e96df67
sariya <dayzwaitik123@yandex.ru>
2024-03-12 22:15:45 +0300

    Handle exp

--- src/weather/router.py
+++ src/weather/router.py
@@ -6,7 +6,11 @@ from datetime import datetime
 
 from src.weather.utils import add_seconds_shift_to_datetime, get_weater_icon_by_description
 from src.weather.service import get_weather_by_city, coords_of_city
-
+from src.weather.exceptions import (
+    WrongWeatherDescriprion,
+    APIWaetherFailed,
+    APIWeatherBadResponse,
+)
 
 logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w",
                     format="%(asctime)s %(levelname)s %(message)s")
@@ -28,20 +32,33 @@ def weather_page(request: Request) -> HTMLResponse:
 def select_city(request: Request, data: str = Form(...)) -> HTMLResponse:
     cities = coords_of_city(data)
     if not cities:
-        return template.TemplateResponse("wrong_city.html", {"request": request, "message": "Такого города нет"})
+        return template.TemplateResponse("error.html", {"request": request, "message": "Такого города нет"})
     return template.TemplateResponse("choice_city.html", {"request": request, "cities": cities})
 
 
 @router.post("/weather_by_city/")
 def get_weather(request: Request, coords: str = Form(...)) -> HTMLResponse:
-    lon, lat = coords.split()
-    weather = get_weather_by_city(lon=lon, lat=lat)
-    now_datetime_utc = datetime.utcnow()
-    date, time = add_seconds_shift_to_datetime(now_datetime_utc, weather.UTC_shift)
-    weather_icon_path = get_weater_icon_by_description(weather.description, time)
-    weather_icon_path = get_weater_icon_by_description(weather.description, time)
-    formatted_date = date.strftime("%d.%m.%Y")
-    formatted_time = time.strftime("%H:%M")
-    return template.TemplateResponse("weather_result.html", {"request": request, "weather": weather, "date": formatted_date, "time": formatted_time, "icon": weather_icon_path})
+    try:
+        lon, lat = coords.split()
+        weather = get_weather_by_city(lon=lon, lat=lat)
+        now_datetime_utc = datetime.utcnow()
+        date, time = add_seconds_shift_to_datetime(now_datetime_utc, weather.UTC_shift)
+        weather_icon_path = get_weater_icon_by_description(weather.description, time)
+        weather_icon_path = get_weater_icon_by_description(weather.description, time)
+        formatted_date = date.strftime("%d.%m.%Y")
+        formatted_time = time.strftime("%H:%M")
+        return template.TemplateResponse(
+            "weather_result.html",
+            {
+                "request": request,
+                "weather": weather,
+                "date": formatted_date,
+                "time": formatted_time,
+                "icon": weather_icon_path
+            }
+        )
+    except (WrongWeatherDescriprion, APIWeatherBadResponse, APIWaetherFailed) as e:
+        logging.error(f"EXP {str(e)}, with parames: {coords}")
+        return template.TemplateResponse("error.html", {"request": request, "message": "Упс, ошибка в API"})
 
 
