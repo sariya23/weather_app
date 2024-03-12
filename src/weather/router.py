e8e107f12cae03fc58624a307d7f573b9e34e529
sariya <dayzwaitik123@yandex.ru>
2024-03-12 21:33:51 +0300

    Pass city name form buttons

--- src/weather/router.py
+++ src/weather/router.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from fastapi.responses import RedirectResponse
-
 import logging
 from datetime import datetime
 
@@ -31,7 +29,14 @@ def weather_page(request: Request) -> HTMLResponse:
     return template.TemplateResponse("no_weather.html", {"request": request})
 
 
-@router.post("/city", response_class=HTMLResponse)
+@router.post("/select_city", response_class=HTMLResponse)
 def weather_city(request: Request, data: str = Form(...)) -> HTMLResponse:
-    return template.TemplateResponse("weather_result.html", {"request": request, "city": data})
+    cities = coords_of_city(data)
+    return template.TemplateResponse("choice_city.html", {"request": request, "cities": cities})
+
+
+@router.post("/weather_by_city/")
+def get_weather(request: Request, city: str = Form(...)) -> HTMLResponse:
+    return template.TemplateResponse("weather_result.html", {"request": request, "city": city})
+
 
