2c3188201a139097ab50910a59caadec2d7574bc
sariya <dayzwaitik123@yandex.ru>
2024-03-12 20:39:03 +0300

    Start to reorgonize html

--- src/weather/router.py
+++ src/weather/router.py
@@ -11,8 +11,7 @@ from src.weather.exceptions import (
     WrongWeatherDescriprion,
 )
 from src.weather.utils import add_seconds_shift_to_datetime, get_weater_icon_by_description
-from src.weather.service import get_weather_by_city
-from src.weather.service import Weather
+from src.weather.service import get_weather_by_city, coords_of_city, Weather
 
 
 logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w",
@@ -28,42 +27,5 @@ template = Jinja2Templates(directory="templates")
     
 @router.get("/", response_class=HTMLResponse)
 def weather_page(request: Request) -> HTMLResponse:
-    return template.TemplateResponse("weather.html", {"request": request})
+    return template.TemplateResponse("base_weather_page.html", {"request": request})
 
-
-@router.post("/", response_class=HTMLResponse)
-def get_weather(request: Request, data: str = Form(...)) -> HTMLResponse:
-    try:
-        weather: Weather = get_weather_by_city(data)
-        now_datetime_utc = datetime.utcnow()
-        date, time = add_seconds_shift_to_datetime(now_datetime_utc, weather.UTC_shift)
-        weather_icon_path = get_weater_icon_by_description(weather.description, time)
-        formatted_date = date.strftime("%d.%m.%Y")
-        formatted_time = time.strftime("%H:%M")
-        return template.TemplateResponse(
-            "weather.html",
-            {
-                "request": request,
-                "weather": weather,
-                "date": formatted_date,
-                "time": formatted_time,
-                "icon": weather_icon_path,
-            }
-        )
-    except APIWeatherBadResponse:
-        message = "Что-то пошло не так, проверьте правильность написания города."
-        return template.TemplateResponse("weather.html", {"request": request, "message": message})
-    except APIWaetherFailed:
-        message = "Ошибка стороннего сервиса"
-        return template.TemplateResponse("weather.html", {"request": request, "message": message})
-    except WrongWeatherDescriprion as e:
-        logging.error(e)
-        message = "Мы не учли все варианты погоды :("
-        return template.TemplateResponse("weather.html", {"request": request, "message": message})
-    except Exception as e:
-        message = "Все сломалось, простите :("
-        logging.error(e)
-        return template.TemplateResponse("weather.html", {"request": request, "message": message})
-
-
- 
