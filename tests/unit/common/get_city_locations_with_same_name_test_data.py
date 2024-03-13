8ff44899772fb473bfc61a6f9558d46007b082e2
sariya <dayzwaitik123@yandex.ru>
2024-03-13 18:46:34 +0300

    Add negative test

--- tests/unit/common/get_city_locations_with_same_name_test_data.py
+++ tests/unit/common/get_city_locations_with_same_name_test_data.py
@@ -3,7 +3,8 @@ from src.weather.service import CityLocation
 POSITIVE_CITY_EXPECTED_LOCATION = (
     ['Анапа', {CityLocation(lat=0.0, lon=0.0, city="Анапа", country="RU")}],
     ['Вашингтон', {CityLocation(lat=0.0, lon=0.0, city="Вашингтон", country="US"), CityLocation(lat=0.0, lon=0.0, city="Washington", country="NL")}],
-    ['анАПА', {CityLocation(lat=0.0, lon=0.0, city="Анапа", country="RU")}]
+    ['анАПА', {CityLocation(lat=0.0, lon=0.0, city="Анапа", country="RU")}],
+    ['Anapa', {CityLocation(lat=0.0, lon=0.0, city="Анапа", country="RU")}]
 )
 
-NEGATIVE_CITY_EXPECTED_LOCATION = ('')
+NEGATIVE_CITY_EXPECTED_LOCATION = ('abobus', '8237489389e', 'NEONAPA')
--- tests/unit/test_get_city_locations_with_same_name.py
+++ tests/unit/test_get_city_locations_with_same_name.py
@@ -1,15 +1,28 @@
 import pytest
 
-from tests.unit.common.get_city_locations_with_same_name_test_data import POSITIVE_CITY_EXPECTED_LOCATION
+from tests.unit.common.get_city_locations_with_same_name_test_data import (
+    POSITIVE_CITY_EXPECTED_LOCATION,
+    NEGATIVE_CITY_EXPECTED_LOCATION,
+)
 from src.weather.service import get_city_locations_with_same_name, CityLocation
+from src.weather.exceptions import WrongCityName
 
 
 @pytest.mark.parametrize("city_name, expected_locations", POSITIVE_CITY_EXPECTED_LOCATION)
-def test_get_city_locations_with_same_names(city_name: str, expected_locations: set[CityLocation]) -> None:
+def test_get_city_locations_with_same_name_positive(city_name: str, expected_locations: set[CityLocation]) -> None:
     """
-    Проверяем, функция переводит название города на русский, если это возможно. И если 
+    Проверяем, что функция переводит название города на русский, если это возможно. И если 
     есть несколько городов с одинаковым названием, то она их все вернет.
     """ 
     cities = get_city_locations_with_same_name(city_name)
     assert cities == expected_locations
+
+
+@pytest.mark.parametrize("city_name", NEGATIVE_CITY_EXPECTED_LOCATION)
+def test_get_city_locations_with_same_name_negative(city_name: str) -> None:
+    """
+    Проверяем, что при неверном вводе города возбуждается исключение. 
+    """ 
+    with pytest.raises(WrongCityName):
+        get_city_locations_with_same_name(city_name)
 
