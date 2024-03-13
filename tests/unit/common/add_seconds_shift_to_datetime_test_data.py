917bd28fabda3a92be9a5ed9dbd0da0f239f3cc6
sariya <dayzwaitik123@yandex.ru>
2024-03-13 19:13:57 +0300

    Add test for add seconds

--- tests/unit/common/add_seconds_shift_to_datetime_test_data.py
+++ tests/unit/common/add_seconds_shift_to_datetime_test_data.py
@@ -1,10 +1,16 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
-_DATETIMES = (
-    datetime(year=1, month=1, day=23, hour=23),
-    datetime(year=1, month=1, day=23, hour=0),
-    datetime(year=1, month=1, day=23, hour=15),
+_datetime1 = datetime(year=1, month=1, day=1, hour=23)
+_datetime2 = datetime(year=1, month=1, day=1, hour=0)
+_datetime3 = datetime(year=2024, month=3, day=13)
+
+_datetime1_shift =  _datetime1 + timedelta(seconds=3600)
+_datetime2_shift = _datetime2 + timedelta(seconds=7200)
+_datetime3_shift = _datetime3 + timedelta(seconds=-3600)
+
+POSTITVE_DATETIME_SHIFT = (
+    [_datetime1, 3600, _datetime1_shift.date(), _datetime1_shift.time()],
+    [_datetime2, 7200, _datetime2_shift.date(), _datetime2_shift.time()],
+    [_datetime3, -3600, _datetime3_shift.date(), _datetime3_shift.time()],
 )
-
-_SHIFT = [i for i in range(0, 86401, 3600)]
--- tests/unit/test_add_seconds_shift_to_datetime.py
+++ tests/unit/test_add_seconds_shift_to_datetime.py
@@ -1,1 +1,17 @@
+import pytest
+
+from datetime import datetime, date, time
+
+from src.weather.utils import add_seconds_shift_to_datetime
+from tests.unit.common.add_seconds_shift_to_datetime_test_data import POSTITVE_DATETIME_SHIFT
+
+
+@pytest.mark.parametrize("datetime_, shift, expected_date, expected_time", POSTITVE_DATETIME_SHIFT)
+def test_add_seconds_to_datetime(datetime_: datetime, shift: int, expected_date: date, expected_time: time) -> None:
+    """
+    Проверяем, что дата и время увеличиваются на переданное кол-во
+    секунд.
+    """
+    new_date, new_time = add_seconds_shift_to_datetime(datetime_, shift)
+    assert new_date == expected_date and new_time == expected_time
 
