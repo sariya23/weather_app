e9f3152b3939bd93b023c1104dd926a5d6281308
sariya <dayzwaitik123@yandex.ru>
2024-03-13 18:35:06 +0300

    Add test data

--- tests/unit/common/get_city_locations_with_same_name_test_data.py
+++ tests/unit/common/get_city_locations_with_same_name_test_data.py
@@ -2,5 +2,8 @@ from src.weather.service import CityLocation
 
 POSITIVE_CITY_EXPECTED_LOCATION = (
     ['Анапа', {CityLocation(lat=0.0, lon=0.0, city="Анапа", country="RU")}],
-    ['Вашингтон', {CityLocation(lat=0.0, lon=0.0, city="Вашингтон", country="US"), CityLocation(lat=0.0, lon=0.0, city="Washington", country="NL")}]
+    ['Вашингтон', {CityLocation(lat=0.0, lon=0.0, city="Вашингтон", country="US"), CityLocation(lat=0.0, lon=0.0, city="Washington", country="NL")}],
+    ['анАПА', {CityLocation(lat=0.0, lon=0.0, city="Анапа", country="RU")}]
 )
+
+NEGATIVE_CITY_EXPECTED_LOCATION = ('')
