cf52448b20e4741e3267c3585d730789db01c81c
sariya <dayzwaitik123@yandex.ru>
2024-03-14 13:30:26 +0300

    Check len after trim string

--- src/static/script/disableButtonWithEmptyInput.js
+++ src/static/script/disableButtonWithEmptyInput.js
@@ -2,7 +2,7 @@ const sendCity = document.getElementById("sendCity");
 const cityInput = document.getElementById("cityInput");
 
 cityInput.oninput = function () {
-    if (cityInput.value.length === 0) {
+    if (cityInput.value.trim().length === 0) {
         sendCity.disabled = true;
     } else {
         sendCity.disabled = false;
