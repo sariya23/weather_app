796c209a6c6166f98929e0fd452c952ef1b40818
sariya <dayzwaitik123@yandex.ru>
2024-03-14 13:26:35 +0300

    Test disable input

--- tests/e2e/weather_page/test_weather_page.py
+++ tests/e2e/weather_page/test_weather_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.common.keys import Keys
 import pytest
 
 from pathlib import Path
@@ -23,3 +24,20 @@ def test_base_image_present(driver: WebDriver, window_size: tuple[int, int]) -> 
     image_src = image_element.get_attribute("src")
     image_from_page = ImageByLink(image_src)
     assert is_two_image_equal(image_from_page.content, Path("../../src/static/images/gopher_info/gopher_play.png"))
+
+
+@pytest.mark.parametrize("window_size", ALL_WINDOW_SIZES)
+def test_send_city_button_disabled(driver: WebDriver, window_size: tuple[int, int]) -> None:
+    """
+    Проверяем, что при определенных условиях, кнопка отправки города заблокировона.
+    Кнопка заблокирована, когда поле ввода города путое.
+    """
+    weather_page = WeatherPage(driver, window_size=window_size)
+    assert weather_page.send_city_button.is_enabled() is False
+
+    weather_page.city_field.send_keys("aboba")
+    weather_page.city_field.send_keys(Keys.CONTROL + "a")
+    weather_page.city_field.send_keys(Keys.DELETE)
+
+    assert weather_page.send_city_button.is_enabled() is False
+
