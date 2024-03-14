9045c07e054c04171e557be10e5979e1d335e17b
sariya <dayzwaitik123@yandex.ru>
2024-03-14 22:28:05 +0300

    trans en

--- tests/e2e/weather_page/common/weather_info_test_data.py
+++ tests/e2e/weather_page/common/weather_info_test_data.py
@@ -1,5 +1,5 @@
 CITY_BUTTON_TO_CLICK_EXPECTED_CITY_NAME = (
     ["Москва RU", "Москва | RU", "Moscow, RU", 3],
     ["Вашингтон DC", "Вашингтон | US", "Washington D.c., US", -4],
-    ["Анапа", "Анапа | RU", "Анапа, RU", 3]
+    ["Анапа", "Анапа | RU", "Anapa, RU", 3]
 )
