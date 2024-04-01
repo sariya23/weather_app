93ed428fce22418213fc32d875d1352fc1c686fe
sariya <dayzwaitik123@yandex.ru>
2024-04-01 18:36:32 +0300

    READDME

--- weather_app/main.py
+++ weather_app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
@@ -10,3 +11,10 @@ app.mount("/src/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(router_info)
 app.include_router(router_weather)
+
+
+def main():
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+if __name__ == "__main__":
+    main()
